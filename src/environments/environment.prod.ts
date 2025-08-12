8f42c196dbd569cc38d5afdf5d9f124c21c74eb1
Shacky1 <meshackvenance7@gamil.com>
2025-08-12 16:50:04 +0300

    Remove environment files from Git tracking

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiKey: '58ad4463a79b77d23d13ac1c8894add4',
-  tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
-};
