b94f45d40cdd962c98ba28f7336ebdfcb7d6b9e6
Shacky1 <meshackvenance7@gamil.com>
2025-08-12 17:46:25 +0300

    Fix environment files for Vercel build (remove process.env usage)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  tmdbApiKey: '', // empty or dummy key
+  tmdbApiKey: '58ad4463a79b77d23d13ac1c8894add4',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500',
+  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
