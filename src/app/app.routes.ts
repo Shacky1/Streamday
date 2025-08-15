135e0ac0dbb8e0f9851183915fd64e6deb5683d3
Shacky1 <meshackvenance7@gamil.com>
2025-08-15 19:03:06 +0300

    Update About Us page with Bootstrap Icons, profile card, hover effects, and environment variable setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MovieDetailComponent } from './pages/movie-detail/movie-detail.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movie/:id', component: MovieDetailComponent },
+  { path: 'about', component: AboutUsComponent },
   { path: '**', redirectTo: '' }
 ];
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
+// environment.ts (development)
 export const environment = {
   production: false,
-  tmdbApiKey: '58ad4463a79b77d23d13ac1c8894add4',
-  tmdbBaseUrl: 'https://api.themoviedb.org/3',
-  tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
+  tmdbApiKey: process.env['TMDB_API_KEY'] || '',
+  tmdbBaseUrl: process.env['TMDB_BASE_URL'] || 'https://api.themoviedb.org/3',
+  tmdbImageBaseUrl: process.env['TMDB_IMAGE_BASE_URL'] || 'https://image.tmdb.org/t/p/w500'
 };
