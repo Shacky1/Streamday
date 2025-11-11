ead003eb63b07aba93e7ed33381f772d6abc4324
shacky1 <meshackvenance99@gmail.com>
2025-11-11 18:18:24 +0300

    changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: false,
   tmdbApiKey: '$TMDB_API_KEY',
   tmdbBaseUrl: '$TMDB_BASE_URL',
-  tmdbImageBaseUrl: '$TMDB_IMAGE_BASE_URL'
+  tmdbImageBaseUrl: '$TMDB_IMAGE_BASE_URL',
 };
